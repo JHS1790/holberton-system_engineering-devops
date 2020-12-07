f1e351249e4bd1cb8719b2f36a45538a938419a0
JHS1790 <1790@holbertonschool.com>
2020-12-07 16:16:05 +0000

    i think 0 is a go

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp
+
+file { 'ntp.conf':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
