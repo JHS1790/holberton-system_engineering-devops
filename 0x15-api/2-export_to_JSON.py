75c5a9db19857c65f69fd20115661ea2c2d5f0d6
JHS1790 <1790@holbertonschool.com>
2021-02-01 16:50:40 +0000

    go at 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""extended Python script to export data in the CSV format."""
+import csv
+from json import dumps
+from requests import get
+from sys import argv
+
+baseURL = "https://jsonplaceholder.typicode.com/"
+user = get(baseURL + "users/" + argv[1])
+todos = get(baseURL + "todos?userId=" + argv[1])
+name = user.json().get("name")
+hidict = {"{}".format(argv[1]): []}
+for task in todos.json():
+    lodict = {"task": "{}".format(task.get("title")),
+              "completed": "{}".format(task.get("completed")),
+              "username": "{}".format(name)}
+    hidict[argv[1]].append(lodict)
+with open('{}.json'.format(argv[1]), mode='w+') as export_json:
+    export_json.write(dumps(hidict))
