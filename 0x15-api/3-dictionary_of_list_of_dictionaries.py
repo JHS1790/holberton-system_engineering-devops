909d97eec1a4131ef763883583adc8acc483314d
JHS1790 <1790@holbertonschool.com>
2021-02-01 21:51:38 +0000

    added main checks to rest

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """extended Python script to export all data in the json format."""
-import csv
-from json import dumps
-from requests import get
+if __name__ == "__main__":
+    import csv
+    from json import dumps
+    from requests import get
 
-baseURL = "https://jsonplaceholder.typicode.com/"
-users = get(baseURL + "users")
-jusers = users.json()
-hidict = {}
-for user in jusers:
-    userID = user.get("id")
-    todos = get(baseURL + "todos?userId=" + str(userID))
-    hidict[userID] = []
-    for task in todos.json():
-        lodict = {"task": "{}".format(task.get("title")),
-                "completed": "{}".format(task.get("completed")),
-                "username": "{}".format(user.get("username"))}
-        hidict[userID].append(lodict)
-with open('todo_all_employees.json', mode='w+') as export_json:
-    export_json.write(dumps(hidict))
+    baseURL = "https://jsonplaceholder.typicode.com/"
+    users = get(baseURL + "users")
+    jusers = users.json()
+    hidict = {}
+    for user in jusers:
+        userID = user.get("id")
+        todos = get(baseURL + "todos?userId=" + str(userID))
+        hidict[userID] = []
+        for task in todos.json():
+            lodict = {"task": "{}".format(task.get("title")),
+                      "completed": "{}".format(task.get("completed")),
+                      "username": "{}".format(user.get("username"))}
+            hidict[userID].append(lodict)
+    with open('todo_all_employees.json', mode='w+') as export_json:
+        export_json.write(dumps(hidict))
