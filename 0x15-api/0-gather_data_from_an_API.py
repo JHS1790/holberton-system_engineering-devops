5332e182a670646717fb2b54f417c7d478023f0e
JHS1790 <1790@holbertonschool.com>
2021-02-01 21:48:45 +0000

    Added main check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,21 +3,22 @@
     a Python script that, using this REST API, for a given employee ID,
     returns information about his/her TODO list progress.
 """
-from requests import get
-from sys import argv
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
 
-baseURL = "https://jsonplaceholder.typicode.com/"
-user = get(baseURL + "users/" + argv[1])
-todos = get(baseURL + "todos?userId=" + argv[1])
-name = user.json().get("name")
-completed = 0
-tasks = 0
-for task in todos.json():
-    if task.get("completed"):
-        completed += 1
-    tasks += 1
-print("Employee {} is done with tasks({}/{}):".format(name, completed, tasks))
-for task in todos.json():
-    if task.get("completed"):
-        print("\t ", end="")
-        print(task.get("title"))
+    baseURL = "https://jsonplaceholder.typicode.com/"
+    user = get(baseURL + "users/" + argv[1])
+    todos = get(baseURL + "todos?userId=" + argv[1])
+    name = user.json().get("name")
+    completed = 0
+    tasks = 0
+    for task in todos.json():
+        if task.get("completed"):
+            completed += 1
+        tasks += 1
+    print("Employee {} is done with tasks({}/{}):".format(name, completed, tasks))
+    for task in todos.json():
+        if task.get("completed"):
+            print("\t ", end="")
+            print(task.get("title"))
