dd2cf31bfdad618c15e54ce3fbefc597356f8601
JHS1790 <1790@holbertonschool.com>
2021-02-01 16:13:04 +0000

    first go at 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    a Python script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+"""
+from sys import argv
+from requests import get
+
+baseURL = "https://jsonplaceholder.typicode.com/"
+user = get(baseURL + "users/" + argv[1])
+todos = get(baseURL + "todos?userId=" + argv[1])
+name = user.json().get("name")
+completed = 0
+tasks = 0
+for task in todos.json():
+    if task.get("completed"):
+        completed += 1
+    tasks += 1
+print("Employee {} is done with tasks({}/{}):".format(name, completed, tasks))
+for task in todos.json():
+    if task.get("completed"):
+        print("\t ", end="")
+        print(task.get("title"))
