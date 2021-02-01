d6bd2103539d8e71a0f50daafc037286929f53a4
JHS1790 <1790@holbertonschool.com>
2021-02-01 21:59:45 +0000

    updated docstring

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""extended Python script to export data in the CSV format."""
+"""extended Python script to export data in the json format."""
 if __name__ == "__main__":
     import csv
     from json import dumps
