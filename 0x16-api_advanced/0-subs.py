aa713f422ea679cb749a6538a738ff597a7be9f2
JHS1790 <1790@holbertonschool.com>
2021-02-04 19:27:42 +0000

    pep8 on 0 again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
    a function that queries the Reddit API and
    returns the number of subscribers
 """
@@ -9,9 +9,10 @@ from requests import get
 def number_of_subscribers(subreddit):
     """ reddit is the big gae """
     try:
-        request = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                 headers={'User-Agent': 'custom'},
-                 allow_redirects=False)
+        request = get('https://www.reddit.com/r/{}/about.json'
+                      .format(subreddit),
+                      headers={'User-Agent': 'custom'},
+                      allow_redirects=False)
         return request.json().get('data').get('subscribers')
     except:
         return 0
