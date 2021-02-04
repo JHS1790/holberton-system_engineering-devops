d4251d8709d2f21e5c8d23c683d787280a6ab504
JHS1790 <1790@holbertonschool.com>
2021-02-04 19:17:17 +0000

    go at 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,9 @@ from requests import get
 def number_of_subscribers(subreddit):
     """ reddit is the big gae """
     try:
-        rq = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        request = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
                  headers={'User-Agent': 'custom'},
                  allow_redirects=False)
-        return rq.json().get('data').get('subscribers')
+        return request.json().get('data').get('subscribers')
     except:
         return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+   a function that queries the Reddit API and
+   prints the titles of the first 10 hot posts
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    try:
+        request = get(
+                      'https://www.reddit.com/r/{}/hot.json?limit=10'
+                      .format(subreddit),
+                      headers={'User-Agent': 'custom'},
+                      allow_redirects=False)
+        for thread in request.json().get('data').get('children'):
+            print(thread.get('data').get('title'))
+    except:
+        print('None')
