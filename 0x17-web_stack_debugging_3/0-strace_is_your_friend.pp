f5311d951091466cee4fafb59a9244c2482ce3eb
JHS1790 <1790@holbertonschool.com>
2021-02-17 16:57:49 +0000

    Simplified sed statement

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
 # Correct Wordpress config misspelling
 exec {'Correct Wordpress misspelling':
-  command  => 'sed -i "s+require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );\
-              +require_once ( ABSPATH . WPINC . \'/class-wp-locale.php\' );+g" \
-              /var/www/html/wp-settings.php',
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   provider => shell,
 }
